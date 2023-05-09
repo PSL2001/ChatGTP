9af16781cf7b956c46768fdb17442c324ecf34c2
PSL2001 <pablitosanchezlopez@gmail.com>
2023-05-09 14:27:06 +0200

    Funcion de texto de API funcionando

--- public/javascripts/cliente.js
+++ public/javascripts/cliente.js
@@ -8,23 +8,107 @@
 // Variables globales
 var button = document.getElementById('btnEnviar');
 var input = document.getElementById('msg');
+var inputError = document.getElementById('api-message');
+
+// Ponemos el foco en el input
+input.focus();
 
 // Ponemos listener
 button.addEventListener('click', enviarMensaje);
+// Ponemos listener al input para que al pulsar enter se envie el mensaje
+input.addEventListener('keyup', function (event) {
+    // Comprobamos que se ha pulsado enter
+    if (event.key === 'Enter' || event.key === 'NumpadEnter') {
+        // Cancelamos el evento por defecto
+        event.preventDefault();
+        // Enviamos el mensaje
+        enviarMensaje();
+        // Ponemos el foco en el input
+        input.focus();
+    }
+});
 
 // Funcion para enviar el mensaje
-function enviarMensaje() {
+async function enviarMensaje() {
+    //Bloqueamos el input y el boton para que no se envien mensajes mientras se procesa el anterior
+    disableUserInput(true);
     //Obtenemos el mensaje
     let msg = input.value;
     // Comprobamos que no este vacio
     if (msg != '') {
+        // Creamos el elemento para mostrar el mensaje
+        crearSpan("<b>Humano: </b>" + msg);
         // Creamos el objeto para enviarlo al servidor
         let data = {
             msg: msg
         }
-        // Llamamos a la funcion del servidor para enviar el mensaje y recibir la respuesta
-        socket.emit('msg', data, function (res) {
-            
-        });
+        // Llamamos al metodo del servidor para enviar el mensaje
+        let res = await fetch('/send-message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data);
+                // Comprobamos que se ha recibido la respuesta
+                if (data) {
+                    crearSpan("<b>IA: </b>" + data);
+                } else {
+                    // Llamamos a la funcion para mostrar el error
+                    mostrarError('No se ha podido procesar la respuesta');
+                }
+            })
+            .catch(err => {
+                // Llamamos a la funcion para mostrar el error
+                mostrarError('No se ha podido enviar el mensaje');
+            });         
     }
+    // Desbloqueamos el input y el boton
+    disableUserInput(false);
+}
+
+// Funcion para crear un span con el mensaje
+function crearSpan(msg) {
+    // Creamos el elemento
+    let span = document.createElement('span');
+    // Le ponemos el texto
+    span.innerHTML = msg;
+    // Le ponemos la clase para que se vea bien
+    span.id = 'user-message'
+    // Lo añadimos al div de mensajes
+    document.getElementById('form').before(span);
+    // Creamos un salto de linea
+    let br = document.createElement('br');
+    // Lo añadimos al div de mensajes
+    document.getElementById('form').before(br);
+    // Ponemos el scroll abajo del todo
+    document.getElementById('form').scrollTop = document.getElementById('form').scrollHeight;
+    //Limpiamos el input
+    input.value = '';
+}
+
+// Funcion para bloquear el input y el boton o desbloquearlos
+function disableUserInput(disable) {
+    // Comprobamos si hay que bloquear o desbloquear
+    if (disable) {
+        // Bloqueamos el input
+        input.disabled = true;
+        // Bloqueamos el boton
+        button.disabled = true;
+    } else {
+        // Desbloqueamos el input
+        input.disabled = false;
+        // Desbloqueamos el boton
+        button.disabled = false;
+    }
+}
+
+// Funcion para mostrar un error en el input de errores
+function mostrarError(msg) {
+    // Ponemos el mensaje
+    inputError.value = msg;
+    // Ponemos el color en rojo
+    inputError.style.color = 'red';
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,89 @@
+'use strict';
+
+require('dotenv').config({ silent: true });
 var express = require('express');
 var router = express.Router();
 
+// Importamos el modulo de ChatGTP
+const { Configuration, OpenAIApi } = require("openai");
+
+// Creamos la configuracion para la peticion
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+
+// Creamos la instancia de OpenAIApi
+const api = new OpenAIApi(configuration);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'ChatAI', message: '¡Habla con nuestra IA!' });
 });
 
+/**
+ * Funcion para enviar el mensaje a la API de OpenAI
+ * @param json data mensaje a enviar
+ * @returns json respuesta de la API
+ * @throws error si no se ha podido enviar el mensaje
+ * @throws error si no se ha podido recibir la respuesta
+ * @throws error si no se ha podido procesar la respuesta
+ */
+router.post('/send-message', function (req, res, next) {
+  // Obtenemos el mensaje que viene en formato JSON
+  let msg = req.body.msg;
+  // Mandamos el mensaje a la API de OpenAI
+  const response = new Promise((resolve, reject) => {
+    let mensaje = api.createCompletion({
+      model: "text-davinci-003",
+      prompt: "La siguiente conversacion es entre un humano y un asistente de IA. Este asistente es muy inteligente, creativo, dispuesto a ayudar y muy amable.\nHumano: Hola, ¿quién eres?\nIA: Soy una IA creada por OpenAI. ¿Cómo puedo ayudarte?\nHumano: " + msg + "\nIA:",
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [" Humano:", " IA:"]
+    }).then((res) => {
+      // Devolvemos la respuesta al cliente
+      resolve(res);
+    }).catch((err) => {
+      // Devolvemos el error al cliente
+      reject(err);
+    });
+
+  //   // Esperamos a que se resuelva la promesa
+  //   mensaje.then((res) => {
+  //     // Comprobamos que es una respuesta valida
+  //     if (res) {
+  //       // Comprobamos que se ha podido procesar la respuesta
+  //       if (res?.choices?.text) {
+  //         // Devolvemos la respuesta al cliente
+  //         resolve({ msg: res.choices.text });
+  //       } else {
+  //         // Devolvemos el error al cliente
+  //         reject(`No se ha podido procesar la respuesta, intentalo mas tarde ${res.statusText} `);
+  //       }
+  //     } else {
+  //       // Devolvemos el error al cliente
+  //       reject('No se ha podido recibir la respuesta');
+  //     }
+  //   }).catch((err) => {
+  //     // Devolvemos el error al cliente
+  //     reject(err);
+  //   });
+ });
+
+
+  // Esperamos a que se resuelva la promesa y enviamos la respuesta al cliente
+  response.then((result) => {
+    console.log(result.data.choices[0].text);
+    res.json(result.data.choices[0].text);
+  }).catch((err) => {
+    console.log(err);
+    res.json({ err: err });
+  });
+});
 
 
 module.exports = router;
