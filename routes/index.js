3befe0a4299bbe4ea53e27d10c29ad097559ec9c
PSL2001 <pablitosanchezlopez@gmail.com>
2023-05-19 09:23:12 +0200

    Limpiado codigo de backend

--- routes/index.js
+++ routes/index.js
@@ -59,161 +59,169 @@ router.get('/', function (req, res, next) {
 });
 
 /**
- * Funcion para enviar el mensaje a la API de OpenAI
- * @param json data mensaje a enviar
- * @returns json respuesta de la API
- * @throws error si no se ha podido enviar el mensaje
- * @throws error si no se ha podido recibir la respuesta
- * @throws error si no se ha podido procesar la respuesta
+ * Función para enviar el mensaje a la API de OpenAI
+ * @param {string} msg Mensaje a enviar
+ * @returns {Promise<string>} Respuesta de la API
  */
-router.post('/send-message', function (req, res, next) {
-  // Obtenemos el mensaje que viene en formato JSON
-  let msg = req.body.msg;
-  console.log(msg);
-  // Mandamos el mensaje a la API de OpenAI
-  const response = new Promise((resolve, reject) => {
-    let mensaje = api.createCompletion({
+async function sendMessage(msg) {
+  try {
+    const prompt = "La siguiente conversacion es entre un humano y un asistente de IA. Este asistente es muy inteligente, creativo, dispuesto a ayudar y muy amable.\nHumano: Hola, ¿quién eres?\nIA: Soy una IA creada por OpenAI. ¿Cómo puedo ayudarte?\nHumano: " + msg + "\nIA:";
+
+    const response = await api.createCompletion({
       model: "text-davinci-003",
-      prompt: "La siguiente conversacion es entre un humano y un asistente de IA. Este asistente es muy inteligente, creativo, dispuesto a ayudar y muy amable.\nHumano: Hola, ¿quién eres?\nIA: Soy una IA creada por OpenAI. ¿Cómo puedo ayudarte?\nHumano: " + msg + "\nIA:",
+      prompt,
       temperature: 0.9,
       max_tokens: 150,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0.6,
       stop: [" Humano:", " IA:"]
-    }).then((res) => {
-      // Devolvemos la respuesta al cliente
-      resolve(res);
-    }).catch((err) => {
-      // Devolvemos el error al cliente
-      reject(err);
     });
- });
 
+    const text = response.data.choices[0].text;
+    return text;
+  } catch (error) {
+    throw error;
+  }
+}
 
-  // Esperamos a que se resuelva la promesa y enviamos la respuesta al cliente
-  response.then((result) => {
-    console.log(result.data.choices[0].text);
-    res.json(result.data.choices[0].text);
-  }).catch((err) => {
-    console.log(err);
-    res.json({ err: err });
-  });
+// Enrutador
+router.post('/send-message', async function (req, res, next) {
+  try {
+    const msg = req.body.msg;
+    console.log(msg);
+    
+    const response = await sendMessage(msg);
+    
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error.message });
+  }
 });
 
-/**
- * Funcion para recibir una imagen de la API de OpenAI, procesarla y devolver la respuesta
- * @param json request del usuario con la imagen
- * @returns json respuesta de la API, la cual contiene una url con la imagen procesada o un error
- * @throws error si no se ha podido enviar la imagen
- * @throws error si no se ha podido recibir la respuesta
- * @throws error si no se ha podido procesar la respuesta
- */
-router.post('/send-image', function (req, res, next) {
-  // Obtenemos la request de la imagen del usuario
-  console.log(req.body);
-  // Pedimos una respuesta a la API de OpenAI
-  const response = new Promise((resolve, reject) => {
-    let imagen = api.createImage({
-      prompt: req.body.prompt,
-      n: req.body.n,
-      size: req.body.size,
-    }).then((res) => {
-      // Devolvemos la respuesta al cliente
-      resolve(res);
-    }).catch((err) => {
-      // Devolvemos el error al cliente
-      reject(err);
-    });
-  });
 
-  // Esperamos a que se resuelva la promesa y enviamos la respuesta al cliente
-  response.then((result) => {
-    console.log(result.data.data);
-    res.json(result.data.data);
-  }).catch((err) => {
-    console.log(err);
-    res.json({ err: err });
-  });
+/**
+ * Función para recibir una imagen de la API de OpenAI, procesarla y devolver la respuesta
+ * @param {Object} request Request del usuario con la imagen
+ * @returns {Promise<Object>} Respuesta de la API que contiene una URL con la imagen procesada o un error
+ */
+async function sendImage(request) {
+  try {
+    const { prompt, n, size } = request;
+    
+    const response = await api.createImage({
+      prompt,
+      n,
+      size,
+    });
+    
+    const responseData = response.data.data;
+    return responseData;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Enrutador
+router.post('/send-image', async function (req, res, next) {
+  try {
+    const request = req.body;
+    console.log(request);
+    
+    const response = await sendImage(request);
+    
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error.message });
+  }
 });
 
+
 /**
- * Funcion para recibir un codigo fuente de la API de OpenAI, procesarlo y devolver la respuesta
- * @param json request del usuario con la petición de código fuente
- * @returns json respuesta de la API, la cual contiene el código fuente procesado o un error
- * @throws error si no se ha podido enviar el código fuente
- * @throws error si no se ha podido recibir la respuesta
- * @throws error si no se ha podido procesar la respuesta
+ * Función para recibir un código fuente de la API de OpenAI, procesarlo y devolver la respuesta
+ * @param {Object} request Request del usuario con la petición de código fuente
+ * @returns {Promise<Object>} Respuesta de la API que contiene el código fuente procesado o un error
  */
-router.post('/send-code', function (req, res, next) {
-  // Obtenemos la request del código fuente del usuario
-  console.log(req.body.msg);
-  // Pedimos una respuesta a la API de OpenAI
-  const response = new Promise((resolve, reject) => {
-    let codigo = api.createCompletion({
+async function sendCode(request) {
+  try {
+    const { msg } = request;
+    
+    const response = await api.createCompletion({
       model: "text-davinci-003",
-      prompt: "Se te ha pedido que " + req.body.msg + ". Escribe solo el codigo fuente que resuelva la peticion, no añadas ni explicacion de este programa o texto antes del codigo fuente",
+      prompt: `Se te ha pedido que ${msg}. Escribe solo el código fuente que resuelva la petición, no añadas ni explicación de este programa o texto antes del código fuente`,
       temperature: 0.9,
       max_tokens: 450,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0.6,
-    }).then((res) => {
-      // Devolvemos la respuesta al cliente
-      resolve(res);
-    }).catch((err) => {
-      // Devolvemos el error al cliente
-      reject(err);
     });
-  });
+    
+    const responseData = response.data.choices[0].text;
+    return responseData;
+  } catch (error) {
+    throw error;
+  }
+}
 
-  // Esperamos a que se resuelva la promesa y enviamos la respuesta al cliente
-  response.then((result) => {
-    console.log(result.data.choices[0].text);
-    res.json(result.data.choices[0].text);
-  }).catch((err) => {
-    console.log(err);
-    res.json({ err: err });
-  });
+// Enrutador
+router.post('/send-code', async function (req, res, next) {
+  try {
+    const request = req.body;
+    console.log(request.msg);
+    
+    const response = await sendCode(request);
+    
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error.message });
+  }
 });
 
+
 /**
- * Funcion para recibir un texto de la API de Watson Assistant, procesarlo y devolver la respuesta
- * @param json request del usuario con el texto
- * @returns json respuesta de la API, la cual contiene el texto procesado o un error
- * @throws error si no se ha podido enviar el texto
- * @throws error si no se ha podido recibir la respuesta
- * @throws error si no se ha podido procesar la respuesta
+ * Función para recibir un texto de la API de Watson Assistant, procesarlo y devolver la respuesta
+ * @param {Object} request Request del usuario con el texto
+ * @returns {Promise<Object>} Respuesta de la API que contiene el texto procesado o un error
  */
-router.post('/send-watson', function (req, res, next) {
-  // Obtenemos la request del texto del usuario
-  console.log(req.body.msg);
-  // Pedimos una respuesta a la API de Watson Assistant
-  const response = new Promise((resolve, reject) => {
-    let answer = assistant.message({
+async function sendWatson(request) {
+  try {
+    const { msg } = request;
+    
+    const response = await assistant.message({
       assistantId: process.env.WATSON_ASSISTANT_ID,
       sessionId: session_id,
       input: {
         'message_type': 'text',
-        'text': req.body.msg
+        'text': msg
       }
-    }).then((res) => {
-      // Devolvemos la respuesta al cliente
-      resolve(res);
-    }).catch((err) => {
-      // Devolvemos el error al cliente
-      reject(err);
     });
-  });
+    
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
 
-  // Esperamos a que se resuelva la promesa y enviamos la respuesta al cliente
-  response.then((result) => {
-    console.log(result);
-    res.json(result);
-  }).catch((err) => {
-    console.log(err);
-    res.json({ err: err });
-  });
+// Enrutador
+router.post('/send-watson', async function (req, res, next) {
+  try {
+    const request = req.body;
+    console.log(request.msg);
+    
+    const response = await sendWatson(request);
+    
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error.message });
+  }
 });
   
 
